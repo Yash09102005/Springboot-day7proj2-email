9b8c7f8f81546b10b828245b141b314c45d744b0
Yash09102005 <yashiladevi09@gmail.com>
2026-06-01 11:22:21 +0530

    Initial commit

---
+++ src/test/java/com/day7proj2/AppTest.java
@@ -0,0 +1,12 @@
+package com.day7proj2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
